4c7afb64213d3a2e805907e2371b0e209f8e9a9d
Ana Karol <151869218+anakarolcatu@users.noreply.github.com>
2024-07-01 19:30:58 -0300

    add: footer component, logo img

---
+++ estudoflix/src/components/Footer/index.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+const Rodape = styled.footer`
+    display: none;
+    @media screen and (min-width: 1024px){
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 125px;
+        background-color: var(--escuro);
+        border-top: var(--border);
+        box-shadow: var(--boxShadow);
+        img {
+            width: 168px;
+        }
+    }
+`
+
+const Footer = () => {
+    return (
+        <Rodape>
+            <img src="/imagens/estudoflix.png" alt="Logo EstudoFlix" />
+        </Rodape>
+    )
+}
+
+export default Footer;
