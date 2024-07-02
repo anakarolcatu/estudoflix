3bcc4d81a91732fe4c83229c6c14f7003cf6e21d
Ana Karol <151869218+anakarolcatu@users.noreply.github.com>
2024-07-02 19:48:30 -0300

    add: started card component

---
+++ estudoflix/src/components/Card/index.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const CardContainer = styled.div`
+    
+`
+
+const Card = () => {
+    return (
+        <CardContainer>
+        </CardContainer>
+    )
+}
+
+export default Card;
